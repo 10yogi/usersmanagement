33bc5dc91abe40140bd02ca63cfc96c7787c879c
Yogesh <kurmiyogi625@gmail.com>
2019-01-09 01:34:49 +0530

    organise modules

--- models/schema/users.js
+++ models/schema/users.js
@@ -1,4 +1,11 @@
-module.exports = {
-  name : Joi.string().min(3).required(),
-  age : Joi.number().required()
-};
+const Joi = require("joi");
+
+module.exports ={
+  schema : {
+    name : Joi.string().min(3).required(),
+    age : Joi.number().required()
+  },
+  validateUser :  function (user){
+    return Joi.validate(user,this.schema);
+  }
+} ;
---
+++ models/users.js
@@ -0,0 +1,14 @@
+module.exports ={
+  users:[
+    {
+      id : 1,
+      name : "Raghav",
+      age : 28
+    },
+    {
+      id : 2,
+      name : "Yogesh",
+      age : 20
+    }
+  ]
+};
